21f08894665340214bf4379351a826784f3d27b5
anushkahebbar <56372418+anushkahebbar@users.noreply.github.com>
2022-11-03 23:38:23 +0530

    Read actions work without issue

--- server/store.go
+++ server/store.go
@@ -13,16 +13,15 @@ import (
 // Clients do not access the store directly. A transaction handler is created
 // per client that holds the store and process id of the validator process.
 
-// An entry is a state: (key, value, unique ref when written)
+// An entry is a state: (Key, Value, unique Ref when written)
 type Entry struct {
-	key   string
-	value string
-	ref   int64
+	Key   string
+	Value string
+	Ref   int64
 }
 
 // A store is a list of entries
 type Store struct {
-	// Include a
 	listener net.Listener
 	data     []Entry
 	len      int
@@ -34,14 +33,14 @@ var ssrv Store
 // Create a type to be able to register functions run by clients on the Store (RPC server)
 type StoreAPI int
 
-// Read the value of the key using the stores's API
+// Read the Value of the Key using the stores's API
 // Return as a new entry object
-func (a *StoreAPI) Read(key string, reply Entry) error {
+func (a *StoreAPI) Read(Key string, reply *Entry) error {
 	for _, entry := range ssrv.data {
-		if entry.key == key {
-			reply.key = entry.key
-			reply.value = entry.value
-			reply.ref = entry.ref
+		if entry.Key == Key {
+			reply.Key = entry.Key
+			reply.Value = entry.Value
+			reply.Ref = entry.Ref
 			break
 		}
 	}
@@ -59,16 +58,11 @@ func main() {
 	n, err := strconv.Atoi(os.Args[2])
 	var temp Entry
 
-	for i := 1; i <= n; i++ {
-		temp = Entry{key: strconv.Itoa(i), value: "0", ref: time.Now().UnixNano()}
-		ssrv.data = append(ssrv.data, temp)
-	}
-
 	api := new(StoreAPI)
 	// Register the StoreAPI object which will be called from the Store (RPC server)
 	err = rpc.Register(api)
 	if err != nil {
-		log.Fatal("error registering Handler API", err)
+		log.Fatal("Error registering Handler API", err)
 	}
 
 	// Register an HTTP handler for RPC messages
@@ -82,9 +76,18 @@ func main() {
 
 	// fmt.Printf("Serving RPC on port %s", os.Args[1])
 	ssrv = Store{listener: listener}
-	http.Serve(ssrv.listener, nil)
 
+	for i := 1; i <= n; i++ {
+		temp = Entry{Key: strconv.Itoa(i), Value: "0", Ref: time.Now().UnixNano()}
+		ssrv.data = append(ssrv.data, temp)
+	}
+
+	// for _, entry := range ssrv.data {
+	// 	fmt.Println(entry.Key, entry.Value, entry.Ref)
+	// }
+
+	http.Serve(ssrv.listener, nil)
 	if err != nil {
-		log.Fatal("error serving: ", err)
+		log.Fatal("Error serving at Store: ", err)
 	}
 }
